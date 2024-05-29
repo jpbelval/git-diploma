5e6e99cd04d70b032361e8acae9588e50314852c
Luka Lepine <lukalego@hotmail.ca>
2024-05-29 13:34:09 -0400

    correction des modeles
    retrait du modele Log
    modification du modele tutor
    ajout modele Event
    modification modele File

---
+++ Backend/backend/src/main/java/com/diploma/git/backend/model/Class.java
@@ -0,0 +1,22 @@
+package com.diploma.git.backend.model;
+
+public class Class {
+    private String sigle;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSigle() {
+        return sigle;
+    }
+
+    public void setSigle(String sigle) {
+        this.sigle = sigle;
+    }
+}
