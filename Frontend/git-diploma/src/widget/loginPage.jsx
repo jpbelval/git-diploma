265f6a13c6ef19fb3a879a6f1073c57751f57079
jeouel <jerome.ouellette@gmail.com>
2024-05-31 15:53:41 -0400

    Create loginPage.jsx

---
+++ Frontend/git-diploma/src/widget/loginPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './studentDashboard.module.css';
+
+const StudentDashboard = () => {
+
+
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default StudentDashboard;
