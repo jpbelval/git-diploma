257202f246a07a305dc9c8ce81ae79e5fe334f35
JP Belval <33385857+jaypey@users.noreply.github.com>
2024-05-31 13:50:29 +0000

    Fix merge

--- Backend/backend/src/main/java/com/diploma/git/backend/StudentController.java
+++ Backend/backend/src/main/java/com/diploma/git/backend/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
