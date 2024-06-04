a7dda9741715f8e7ffbe5925b55c859b97f46ba9
JP Belval <jpbelv@gmail.com>
2024-06-03 21:13:48 -0400

    Add Gitolite

--- GitServer/docker-entrypoint.sh
+++ GitServer/docker-entrypoint.sh
@@ -1,39 +1,41 @@
 #!/bin/sh
 
-set -eu
+# if command is sshd, set it up correctly
+if [ "${1}" = 'sshd' ]; then
+  set -- /usr/sbin/sshd -D
 
-if [ -n "${DEBUG-}" ]; then set -x; fi
-
-script_name="$0"
-
-msg() { echo >&2 -e "${script_name}:" "${1-}"; }
-warn() { msg "warning: ${1-}"; }
-die() {   msg "error: ${1-}"; exit "${2-1}"; }
-
-entrypoint_dir="/docker-entrypoint.d"
-
-if [ -d "$entrypoint_dir" ]; then
-    msg "Starting container configuration"
-else
-    die "Configuration directory '${entrypoint_dir}' not found" 64
+  # Setup SSH HostKeys if needed
+  for algorithm in rsa dsa ecdsa ed25519
+  do
+    keyfile=/etc/ssh/keys/ssh_host_${algorithm}_key
+    [ -f $keyfile ] || ssh-keygen -q -N '' -f $keyfile -t $algorithm
+    grep -q "HostKey $keyfile" /etc/ssh/sshd_config || echo "HostKey $keyfile" >> /etc/ssh/sshd_config
+  done
+  # Disable unwanted authentications
+  perl -i -pe 's/^#?((?!Kerberos|GSSAPI)\w*Authentication)\s.*/\1 no/; s/^(PubkeyAuthentication) no/\1 yes/' /etc/ssh/sshd_config
+  # Disable sftp subsystem
+  perl -i -pe 's/^(Subsystem\ssftp\s)/#\1/' /etc/ssh/sshd_config
 fi
 
-find "/docker-entrypoint.d/" -follow -type f -print | \
-    sort -V | \
-    while read -r f; do
-        case "$f" in
-            *.sh)
-                if [ -x "$f" ]; then
-                    msg "Running $f"
-                    "$f"
-                else
-                    warn "Ignored '$f', not executable"
-                fi
-                ;;
-            *) warn "Ignored '$f'" ;;
-        esac
-    done
+# Fix permissions at every startup
+chown -R git:git ~git
 
-msg "Container configuration completed"
+# Setup gitolite admin  
+if [ ! -f ~git/.ssh/authorized_keys ]; then
+  if [ -n "$SSH_KEY" ]; then
+    [ -n "$SSH_KEY_NAME" ] || SSH_KEY_NAME=admin
+    echo "$SSH_KEY" > "/tmp/$SSH_KEY_NAME.pub"
+    su - git -c "gitolite setup -pk \"/tmp/$SSH_KEY_NAME.pub\""
+    rm "/tmp/$SSH_KEY_NAME.pub"
+  else
+    echo "You need to specify SSH_KEY on first run to setup gitolite"
+    echo "You can also use SSH_KEY_NAME to specify the key name (optional)"
+    echo 'Example: docker run -e SSH_KEY="$(cat ~/.ssh/id_rsa.pub)" -e SSH_KEY_NAME="$(whoami)" jgiannuzzi/gitolite'
+    exit 1
+  fi
+# Check setup at every startup
+else
+  su - git -c "gitolite setup"
+fi
 
 exec "$@"
