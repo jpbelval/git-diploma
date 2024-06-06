5adf109a246111a38ef06956f5bab5922b547e31
Luka Lepine <lukalego@hotmail.ca>
2024-06-06 09:48:21 -0400

    TeamController et TeamMapper

---
+++ Backend/backend/src/main/java/com/diploma/git/backend/TeamController.java
@@ -0,0 +1,56 @@
+package com.diploma.git.backend;
+
+import com.diploma.git.backend.mapper.TeamMapper;
+import com.diploma.git.backend.model.Project;
+import com.diploma.git.backend.model.Student;
+import org.apache.ibatis.annotations.Delete;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/team")
+@CrossOrigin
+public class TeamController {
+    @Autowired
+    TeamMapper teamMapper;
+
+    @GetMapping("/getProjects")
+    public List<Project> getTeams(@RequestParam(value = "sigle") String sigle) {
+        List<Project> teams = teamMapper.getProjectsFromCourse(sigle);
+        for(int i = 0; i < teams.size(); i++)
+            teams.get(i).setStudents(teamMapper.getStudentsFromProject(teams.get(i).getId_project()));
+        return teams;
+    }
+
+    @GetMapping("/RegisterInProject")
+    public boolean RegisterInTeam(@RequestParam(value = "sigle") String sigle,
+                                  @RequestParam(value = "id_project") String id_project,
+                                  @RequestParam(value = "cip") String cip) {
+        if (!IsInProject(sigle, cip))
+            return false;
+        teamMapper.registerStudentInProject(id_project, cip);
+        return true;
+    }
+
+    @GetMapping("/IsInProject")
+    public boolean IsInProject(@RequestParam(value = "sigle") String sigle,
+                               @RequestParam(value = "cip") String cip) {
+        List<Project> teams = getTeams(sigle);
+        for (int i = 0; i < teams.size(); i++) {
+            List<Student> students = teams.get(i).getStudents();
+            for (int j = 0; j < students.size(); j++)
+                if (students.get(i).getCip().equals(cip))
+                    return true;
+        }
+        return false;
+    }
+
+    /*
+    @GetMapping("/getProjects")
+    public List<Project> getProjects(@RequestParam(value = "cip") String cip) {
+        return studentMapper.getProjectsFromStudent(cip);
+    }
+     */
+}
