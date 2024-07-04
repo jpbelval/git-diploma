491049e558246282d15ae5d6af2a6b999147ab3f
Laurent Brochu <laurentbrochu2003@gmail.com>
2024-07-04 10:13:39 -0400

    requesthandlers

---
+++ Backend/backend/src/main/java/com/diploma/git/backend/JwtConverterProperties.java
@@ -0,0 +1,31 @@
+package com.diploma.git.backend;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+//@Data
+@Validated
+@Configuration
+@ConfigurationProperties(prefix = "jwt.auth.converter")
+public class JwtConverterProperties {
+
+    private String resourceId;
+    private String principalAttribute;
+
+    public String getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(String resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public String getPrincipalAttribute() {
+        return principalAttribute;
+    }
+
+    public void setPrincipalAttribute(String principalAttribute) {
+        this.principalAttribute = principalAttribute;
+    }
+}
