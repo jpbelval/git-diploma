865eb671e1c3b52b2cfadaf1ffcae0797859250b
jeouel <ouej2018@usherbrooke.ca>
2024-07-07 18:51:16 -0400

    Update to project page
    docker compose version no longer necessary, commented
    Project Details frontend main widget started

--- Frontend/git-diploma/src/widget/projectDetails.jsx
+++ Frontend/git-diploma/src/widget/projectDetails.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {projet} from './data.js'
 import { useParams } from "react-router-dom";
+import axios from 'axios';
+import styles from './styles.module.css';
+
 
 const ProjectDetails = () => {
     let p = useParams();
     let pId = parseInt(p.projectId);
     const proj = projet.filter(projet => projet.projectId == pId);
+
+    const [project, setProject] = useState([]);
+
+    useEffect(() => {
+        const fetchProjet = async () => {
+            const response = await axios.get('/api/repository/project')
+            setProject(response.data)
+        };
+        fetchProjet();
+    }, []);
+
 
     const infoProjet = proj.map(proj => 
         <p>{proj.name}</p>
@@ -13,7 +27,19 @@ const ProjectDetails = () => {
 
     return (
         <>
-        {infoProjet}
+        <div className={styles.projectContainer}>
+            <h2>Repository Files</h2>
+            <div className={styles.projectFileList}>
+                {project.map(project => (
+                    <div className={styles.projectItem} key={project.path}>
+                        <div className={styles.projectPath}>{file.path}</div>
+                        <div className={styles.projectSize}>{file.size} bytes</div>
+                        <div className={styles.projectUser}>{file.user}</div>
+                        <div className={styles.projectDate}>{new Date(file.date).toLocaleString()}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
         </>
     )
 };
